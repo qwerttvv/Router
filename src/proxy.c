8ab83778b645439f64eb16b6580a8f3422a5e8ad
Philipp Wollermann <philwo@google.com>
2024-12-27 15:00:38 +0900

    Fix a broken for-loop in proxy_set
    The semicolon at the end of the for-loop in proxy_set caused the loop to
    iterate over all downlinks, without executing the body of the loop.

--- src/proxy.c
+++ src/proxy.c
@@ -158,7 +158,7 @@ int proxy_set(int uplink, const int downlinks[], size_t downlinks_cnt, enum prox
 		struct querier_user_iface *iface = container_of(user, struct querier_user_iface, user);
 
 		size_t i;
-		for (i = 0; i < downlinks_cnt && downlinks[i] == iface->iface->ifindex; ++i);
+		for (i = 0; i < downlinks_cnt && downlinks[i] == iface->iface->ifindex; ++i)
 			if (i == downlinks_cnt)
 				proxy_remove_downlink(container_of(iface, struct proxy_downlink, iface));
 	}
