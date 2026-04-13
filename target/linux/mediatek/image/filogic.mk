b52da5d88e423bd224eb8b1f0573c3e6fc701683
Tianling Shen <cnsztl@immortalwrt.org>
2026-04-13 14:55:49 +0800

    mediatek: add Ruijie RG-X60 support
    Hardware specification:
      SoC: MediaTek MT7986A 4x A53
      Flash: 128MB SPI-NAND (W25N01GVZEIG)
      RAM: 512MB DDR3 (W634GU6QB)
      Ethernet: 3x 1GbE (MT7531) + 1x 2.5GbE (EN8811H)
      WiFi: MediaTek MT7975(P)N
      Button: Mesh, Reset
      Power: DC Jack 12V
    
    Flash instructions:
    Login into the webUI and flash sysupgrade.bin without saving
    configurations.
    
    While at it, create a shared dtsi for X60 and X60-Pro devices and
    assign MAC using NVMEM as much as possible. The SPI frequcency has
    been increased to 52 MHz as well since the chip supports up to
    108 MHz.
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -2966,6 +2966,18 @@ define Device/routerich_be7200
 endef
 TARGET_DEVICES += routerich_be7200
 
+define Device/ruijie_rg-x60
+  DEVICE_VENDOR := Ruijie
+  DEVICE_MODEL := RG-X60
+  DEVICE_DTS := mt7986a-ruijie-rg-x60
+  DEVICE_DTS_CONFIG := config@ruijie_x60_gsw_en8811h_phy
+  DEVICE_DTS_DIR := ../dts
+  SUPPORTED_DEVICES += X60
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7986-firmware mt7986-wo-firmware kmod-phy-airoha-en8811h
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += ruijie_rg-x60
+
 define Device/ruijie_rg-x60-pro
   DEVICE_VENDOR := Ruijie
   DEVICE_MODEL := RG-X60 Pro
