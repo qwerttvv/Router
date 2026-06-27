27955135be8b946992b119626dee6d91ad1e8152
VIKING <viking.yfy@qq.com>
2026-06-28 00:37:36 +0800

    update qca-nss script

--- target/linux/qualcommax/base-files/etc/uci-defaults/15_smp_affinity.sh
+++ target/linux/qualcommax/base-files/etc/uci-defaults/15_smp_affinity.sh
@@ -7,6 +7,10 @@ uci -q batch << EOF
   set smp_affinity.general=smp_affinity
   set smp_affinity.general.enable='1'
   set smp_affinity.general.enable_log='1'
+  set smp_affinity.netdev=netdev
+  set smp_affinity.netdev.enable='1'
+  set smp_affinity.netdev.rps_flow_cnt='8192'
+  set smp_affinity.netdev.rps_sock_flow_entries='65535'
   commit smp_affinity
 EOF
 
