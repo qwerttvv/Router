9e08f0c3cf44c6681d7f9b7730adf3fad8db62a5
VIKING <viking.yfy@qq.com>
2026-06-28 14:17:16 +0800

    cleanup qca-nss script

--- package/kernel/mac80211/ath.mk
+++ package/kernel/mac80211/ath.mk
@@ -355,10 +355,6 @@ This module adds support for Qualcomm Technologies 802.11ax family of
 chipsets.
 endef
 
-define KernelPackage/ath11k/conffiles
-/etc/config/pbuf
-endef
-
 define KernelPackage/ath11k/config
 
        config ATH11K_THERMAL
---
+++ target/linux/qualcommax/base-files/etc/uci-defaults/15_nss_cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm -f \
+	/etc/config/ecm \
+	/etc/config/nss \
+	/etc/config/pbuf \
+	/etc/config/skb_recycler \
+	/etc/config/smp_affinity
+
+exit 0
